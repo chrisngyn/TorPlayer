9918a23f89ea50a719c83239b9ded57ca41bc3a3
ChienNM3 <chien.nm196@gmail.com>
2023-12-31 22:19:12 +0700

    Implement logic to upload torrent file

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,4 +1,52 @@
 <script setup lang="ts">
+import { computed, ref } from "vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+interface HTMLInputEvent extends Event {
+  target: HTMLInputElement & EventTarget;
+}
+
+const textInput = ref("");
+const fileInput = ref<File | null>(null);
+
+const onTextInputChange = (e: Event) => {
+  textInput.value = (e.target as HTMLInputElement).value;
+  fileInput.value = null;
+};
+
+const onFileInputChange = (ev: HTMLInputEvent | DragEvent) => {
+  let files = (ev as HTMLInputEvent).target.files || (ev as DragEvent).dataTransfer?.files;
+  if (!files?.length) {
+    fileInput.value = null;
+    return;
+  }
+
+  textInput.value = "";
+  fileInput.value = files[0];
+};
+
+const getInfo = async () => {
+  if (textInput.value) {
+    if (textInput.value.startsWith("magnet")) {
+      console.log("magnet");
+    } else if (textInput.value.startsWith("http")) {
+      console.log("http");
+    } else {
+      console.log("hash");
+    }
+  } else if (fileInput.value) {
+    console.log("file");
+    const arrBuf = await fileInput.value.arrayBuffer();
+    console.log(new TextDecoder("utf-8").decode(arrBuf));
+  }
+  // await router.push({ name: "video", params: { id: "123" } });
+};
+
+const disableGetInfo = computed<boolean>(() => {
+  return !textInput.value && !fileInput.value;
+});
 
 </script>
 
@@ -13,6 +61,9 @@
         type="text"
         id="textInput"
         name="textInput"
+        :value="textInput"
+        @input="onTextInputChange"
+        autocomplete="off"
         placeholder="Enter magnet, hash or torrent file link"
         class="p-4 rounded border border-gray-400 w-full bg-stone-700 text-stone-100 hover:border-red-700 hover:bg-stone-800"
       />
@@ -20,13 +71,14 @@
       <label for="fileInput" class="block text-sm my-2">Or Upload Torrent File</label>
       <input
         type="file"
-        id="fileInput"
         name="fileInput"
         accept=".torrent"
+        @input="onFileInputChange"
         class="border text-stone-100 border-gray-400 hover:border-red-700 file:mr-5 rounded bg-stone-700 file:border-[0px] file:p-4 file:bg-stone-700 file:rounded-l file:text-stone-100 hover:file:cursor-pointer hover:file:bg-stone-800 hover:file:text-red-700"
       />
-      <button class="px-4 py-2 my-3 bg-red-600 rounded" @click="$router.push({name:'video', params: {id: '123'}})">
-        Play
+      <button class="px-4 py-2 my-3 bg-red-600 rounded disabled:bg-stone-500" @click="getInfo"
+              :disabled="disableGetInfo">
+        Get Info
       </button>
     </div>
   </main>
