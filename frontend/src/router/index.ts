0fea0b7cd516dc0783fe205b77e43e2ff259fa6d
ChienNM3 <chien.nm196@gmail.com>
2024-01-01 10:55:25 +0700

    Rename PlayerView to WatchView

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -17,7 +17,7 @@ const router = createRouter({
     {
       path: "/t/:infoHash/watch/:fileName",
       name: "watch",
-      component: () => import("@/views/PlayerView.vue"),
+      component: () => import("@/views/WatchView.vue"),
     },
     {
       path: "/about",
@@ -25,7 +25,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
+      component: () => import("@/views/AboutView.vue"),
     },
   ],
 });
