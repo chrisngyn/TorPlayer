7552f2897f601bf848056bde41da7e98ccdd4248
ChienNM3 <chien.nm196@gmail.com>
2024-01-04 01:01:27 +0700

    List and apply subtitles

--- frontend/src/views/WatchView.vue
+++ frontend/src/views/WatchView.vue
@@ -1,15 +1,16 @@
 <script setup lang="ts">
 import { useRoute } from "vue-router";
-import toWebVTT from "srt-webvtt";
-import { onMounted, onUnmounted } from "vue";
+import { onMounted, onUnmounted, ref } from "vue";
 import { GetTorrentInfo, StartDownload, StopDownload } from "../../wailsjs/go/main/App";
 import BackButton from "@/components/BackButton.vue";
+import toWebVTT from "srt-webvtt";
 
 const route = useRoute();
 const infoHash = typeof route.params.infoHash == "string" ? route.params.infoHash : route.params.infoHash[0];
 const fileName = typeof route.params.fileName == "string" ? route.params.fileName : route.params.fileName[0];
 
 const videoSrc = encodeURI(`/stream/${infoHash}/file/${fileName}`);
+const subtitles = ref<{ label: string; url: string; }[]>([]);
 
 onMounted(async () => {
   console.log(`Starting download infoHash: ${infoHash}, fileName: ${fileName}`);
@@ -18,13 +19,10 @@ onMounted(async () => {
   console.log("Starting download subtitles");
   const info = await GetTorrentInfo(infoHash);
   const subtitleFiles = info.files.filter((file) => file.displayPath.endsWith(".srt"));
-  let i = 0;
-  for (const file of subtitleFiles) {
-    console.log(`Downloading subtitle ${file.displayPath}`);
-    const textTrackUrl = await normalizeSubtitleURL(file.displayPath);
-    appendSubtitle(file.displayPath, textTrackUrl, i === 0);
-    i++;
-  }
+  for (const file of subtitleFiles) subtitles.value.push({
+    label: file.displayPath,
+    url: encodeURI(`/stream/${infoHash}/file/${file.displayPath}`),
+  });
 });
 
 onUnmounted(async () => {
@@ -32,22 +30,27 @@ onUnmounted(async () => {
   await StopDownload(infoHash, fileName);
 });
 
-const normalizeSubtitleURL = async (fileName: string): Promise<string> => {
-  const resp = await fetch(encodeURI(`/stream/${infoHash}/file/${fileName}`));
-  const data = await resp.blob();
-  return await toWebVTT(data);
-};
+async function changeSubtitle(label: string, url: string) {
+  console.log(`Changing subtitle to ${label}, ${url}`);
+  removeSubtitle();
 
-const appendSubtitle = (label: string, textTrackUrl: string, isDefault: boolean) => {
+  const resp = await fetch(url);
+  const textTrackUrl = await toWebVTT(await resp.blob());
+
   const textTrack = document.createElement("track");
   textTrack.kind = "subtitles";
   textTrack.label = label;
   textTrack.srclang = "en";
   textTrack.src = textTrackUrl;
-  textTrack.default = isDefault;
+  textTrack.default = true;
   const video = document.querySelector("video");
   video?.appendChild(textTrack);
-};
+}
+
+function removeSubtitle() {
+  const video = document.querySelector("video");
+  video?.querySelectorAll("track").forEach((track) => video.removeChild(track));
+}
 
 
 </script>
@@ -64,5 +67,15 @@ const appendSubtitle = (label: string, textTrackUrl: string, isDefault: boolean)
         type="video/mp4"
       />
     </video>
+    <div>
+      <template v-for="sub in subtitles" :key="sub.url">
+        <button
+          class="bg-stone-900 hover:bg-stone-800 text-slate-100 px-4 py-2 m-2 rounded"
+          @click="changeSubtitle(sub.label, sub.url)"
+        >
+          {{ sub.label }}
+        </button>
+      </template>
+    </div>
   </main>
 </template>
